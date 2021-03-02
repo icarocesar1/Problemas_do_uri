b5919255f5d965f43b3285be0ffea0784584181a
icarocesar1 <72035461+icarocesar1@users.noreply.github.com>
2021-03-02 17:03:22 -0300

    Create 1001_Extremamente_Básico.c

---
+++ 1001_Extremamente_Básico.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+
+   int a;
+   int b;
+   int x;
+   scanf("%i",&a);
+   scanf("%i",&b);
+   x=a+b;
+   printf("X = %d\n",x);
+   
+    
+     
+ 
+    return 0;
+}
