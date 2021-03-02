5f01c9c39561e0500b7bb8fb7843dcc768670058
icarocesar1 <72035461+icarocesar1@users.noreply.github.com>
2021-03-02 17:08:10 -0300

    Create  1003_Soma_Simples.c

---
+++ 1003_Soma_Simples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A;
+    int B;
+    int soma;
+    scanf("%i",&A);
+    scanf("%i",&B);
+    soma= A+B;
+    printf("SOMA = %i\n",soma);
+    return 0;
+ 
+   
+}
