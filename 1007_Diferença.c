9d9b00f7b0f98dc5586257cf1374b5c3bb84a761
icarocesar1 <72035461+icarocesar1@users.noreply.github.com>
2021-03-11 14:26:23 -0300

    Create 1007_Diferença.c

---
+++ 1007_Diferença.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A;
+    int B;
+    int C;
+    int D;
+    int diferenca;
+    scanf("%i",&A);
+    scanf("%i",&B);
+    scanf("%i",&C);
+    scanf("%i",&D);
+    diferenca=(A*B - C*D);
+    printf("DIFERENCA = %i\n",diferenca);
+
+    return 0;
+}
