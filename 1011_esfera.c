7370e1b25447986e9106c32e0d09d2686abd91b4
icarocesar1 <72035461+icarocesar1@users.noreply.github.com>
2021-03-11 14:29:44 -0300

    Create 1011_esfera.c

---
+++ 1011_esfera.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double raio;
+    double pi=3.14159;
+    double volume;
+    scanf("%lf",&raio);
+    volume=(4.0/3)*pi*raio*raio*raio;
+    printf("VOLUME = %.3lf\n",volume);
+    return 0;
+ 
+    
+}
