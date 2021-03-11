8265d557b512fe9c029fc627d610eb755b34de3a
icarocesar1 <72035461+icarocesar1@users.noreply.github.com>
2021-03-11 14:20:30 -0300

    Create 1004_produto_simples.c

---
+++ 1004_produto_simples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a;
+    int b;
+    int prod;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    prod=a*b;
+    printf("PROD = %i\n",prod);
+
+    return 0;
+ 
+   
+}
