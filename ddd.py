90dae5284ce19996b5a4755f3330039e8818cdcb
Ícaro César <72035461+icarocesar1@users.noreply.github.com>
2022-07-26 20:03:11 -0300

    Create ddd.py
    DDD  de alguns estados brasileiros.

---
+++ ddd.py
@@ -0,0 +1,20 @@
+
+numero_regiao =int(input())
+if numero_regiao == 61:
+    print('Brasilia')
+elif numero_regiao == 71:
+    print('Salvador')
+elif numero_regiao == 11:
+    print('Sao Paulo')
+elif numero_regiao == 21:
+    print('Rio de Janeiro')
+elif numero_regiao == 32:
+    print('Juiz de Fora')
+elif numero_regiao == 19:
+    print('Campinas')
+elif numero_regiao == 27:
+    print('Vitoria')
+elif numero_regiao == 31:
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
